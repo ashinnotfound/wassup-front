8967271ceee07487aade4c7a7c56b4eca89653fc
ashinnotfound <ashinnotfound@qq.com>
2024-04-06 22:33:41 +0800

    feat: 视频自动重播

--- lib/widget/component/video_player.dart
+++ lib/widget/component/video_player.dart
@@ -24,6 +24,7 @@ class _VideoAppState extends State<VideoApp> {
             ? Url.createObjectUrl(Blob([widget.videoBytes]))
             : widget.url!))
       ..initialize().then((_) {
+        _controller.setLooping(true);
         setState(() {});
       });
   }
