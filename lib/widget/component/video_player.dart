290506e825bd207f443c6b838b5adbbfec708082
ashinnotfound <ashinnotfound@qq.com>
2024-04-06 20:30:15 +0800

    refactor: 删去无用代码

--- lib/widget/component/video_player.dart
+++ lib/widget/component/video_player.dart
@@ -4,14 +4,6 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
-void main() {
-  runApp(VideoApp(
-    videoBytes: [] as Uint8List,
-    url:
-        "https://wassup.ashinnotfound.top/minio/wassup/563697a4007c8208b9b3410c5fe7725e",
-  ));
-}
-
 class VideoApp extends StatefulWidget {
   final Uint8List? videoBytes;
   final String? url;
